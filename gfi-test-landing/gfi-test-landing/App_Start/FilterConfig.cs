afc370a52e86ea6ee06abd593c76cc044cfe0726
daniel.domingues <daniel.domingues@GFI.pt>
2018-07-13 12:29:09 +0100

    asd

---
+++ gfi-test-landing/gfi-test-landing/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace gfi_test_landing
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ gfi-test-landing/gfi-test-landing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gfi_test_landing.Startup))]
+namespace gfi_test_landing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
