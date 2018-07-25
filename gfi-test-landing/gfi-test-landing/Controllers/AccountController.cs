4531b7b1286844d190d568c03879cb8bba45fcc3
Cláudio Costa <claudio.costa@gfi.pt>
2018-07-25 16:37:33 +0100

    changes

--- gfi-test-landing/gfi-test-landing/Controllers/AccountController.cs
+++ gfi-test-landing/gfi-test-landing/Controllers/AccountController.cs
@@ -194,7 +194,7 @@ namespace gfi_test_landing.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Dashboard", "Home");
                 }
                 AddErrors(result);
             }
