2036d0389eae8febd26bb0f6f8b8e7e38bfae592
daniel.domingues <daniel.domingues@GFI.pt>
2018-08-07 11:16:07 +0100

    chart

--- gfi-test-landing/gfi-test-landing/Controllers/HomeController.cs
+++ gfi-test-landing/gfi-test-landing/Controllers/HomeController.cs
@@ -28,33 +28,37 @@ namespace gfi_test_landing.Controllers
         {
             changeLanguage(language);
 
-            List<String> broswer = new List<String>();
-            int chrome = 0;
-            int firefox = 0;
-            foreach (var item in db.Test)
-            {
-                broswer.Add(item.broswer);
-                if (item.broswer == "Chrome")
-                {
-                    chrome++;
-                    ViewBag.brw1 = "Chrome";
-                }
-                    
-                if (item.broswer == "Firefox")
-                {
-                    firefox++;
-                    ViewBag.brw2 = "Firefox";
-                }
-                    
-            }
-            ViewBag.brwChrome = chrome;
-            ViewBag.Firefox = firefox;
-            ViewBag.broswer = broswer;
-            return View();
+            //var chrome = db.Test.Where(x=> x.broswer=="Chrome").Count();
+            //var firefox = db.Test.Where(x => x.broswer == "Firefox").Count();
+            //var ie = db.Test.Where(x => x.broswer == "IE").Count();
+            //var edge = db.Test.Where(x => x.broswer == "Edge").Count();
+            //var opera = db.Test.Where(x => x.broswer == "Opera").Count();
 
+            //Brand obj = new Brand();
+            //obj.Chrome = chrome.ToString();
+            //obj.Firefox = firefox.ToString();
+            //obj.IE = ie.ToString();
+            //obj.Edge = edge.ToString();
+            //obj.Opera = opera.ToString();
+
+
+
+            //return Json(obj,JsonRequestBehavior.AllowGet);
+            return View();
             //https://www.youtube.com/watch?v=20L-h1rKyvM
             //https://www.youtube.com/watch?v=AqayTPADGsg
         }
+
+        public class Brand
+        {
+            public string Chrome { get; set; }
+            public string Firefox { get; set; }
+            public string IE { get; set; }
+            public string Edge { get; set; }
+            public string Opera { get; set; }
+        }
+
+
         [Authorize]
         public ActionResult Index(string language)
         {
